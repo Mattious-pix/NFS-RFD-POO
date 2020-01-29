9e714e7a7ae4b6db2f7563a62f14c53ea3008f7d
Mattious-pix <ed.deline90@gmail.com>
2020-01-29 11:28:19 +0100

    Commit pour energie cinetique

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -2,9 +2,26 @@
 
 class Voiture
 {
-  public $nbrRouez;
+  public $nbrRoues;
   public $nbrPlaces;
   public $masse;
   public $vitesse = 0;
+  public $couleur;
 
-} ?>
+public function __construct(float $m, string $c)
+{
+  $this->masse = abs($m);
+  $this->couleur = $c;
+
+}
+
+PUBLIC FUNCTION calculerEnergieCinetique()
+{
+  return 0.5 * $this->masse * $this->vitesse **2;
+}
+  public function AfficherUnMessageDebile()
+  {
+    echo "Je roule en caisse";
+  }
+
+}
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require_once './classes/Voiture.php';
 
-$voiture1 = new Voiture();
+$voiture1 = new Voiture(1000, 'rouge');
 
+$voiture1 -> AfficherUnMessageDebile();
 var_dump($voiture1);
